8d2a59b8fd39328511eac4544e8156fed0853614
gigayulianti <yulianti2@gmail.com>
2026-05-26 11:40:27 +0700

    commit

--- circularQueue.cpp
+++ circularQueue.cpp
@@ -163,4 +163,16 @@ int main()
             return 0;
         }
         default:
-        
+        {
+            cout << "Invalid option!!!" << endl;
+            break;
+        }
+        }
+    }
+    catch (exception &e)
+    {
+        cout << "check for the values entered." << endl;
+    }
+}
+return 0;
+}  
