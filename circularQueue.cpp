5ab7fbeba4a5653d41fdcc61eca179aa1b7b2c5b
gigayulianti <yulianti2@gmail.com>
2026-05-26 11:28:48 +0700

    coomit 2

--- circularQueue.cpp
+++ circularQueue.cpp
@@ -9,4 +9,15 @@ class Queues
 private:
     static const int max = 3;
     int FRONT, REAR;
-   
+   int queue_array[max];
+
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert()
+    {
+       
