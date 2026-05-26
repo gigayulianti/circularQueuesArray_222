57dbd3e3ca23b9ee76bf8768f73e2bd17f6b0c2a
gigayulianti <yulianti2@gmail.com>
2026-05-26 11:31:47 +0700

    commit

--- circularQueue.cpp
+++ circularQueue.cpp
@@ -42,4 +42,15 @@ public:
         {
         // jika rear berada di posisi terakhir array, kembali ke awal array
         if (REAR == max - 1)
-            
+            REAR = 0;
+        else
+            REAR = REAR + 1
+        }
+    queue_array[REAR] = num;
+    }
+
+    void remove()
+    {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        
